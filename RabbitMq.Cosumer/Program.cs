ba8a71ee6041da1017e019b21d1f488041836821
Yeon-Taek Choi <ohe@cokee.io>
2022-10-14 22:30:12 +0900

    optimized for connecting game server

--- RabbitMq.Cosumer/Program.cs
+++ RabbitMq.Cosumer/Program.cs
@@ -6,17 +6,21 @@ using RabbitMQ.Client.Events;
 Console.WriteLine("Connecting . . .");
 var factory = new ConnectionFactory
 {
-    // Uri = new Uri("amqps://b-9c19991c-d773-47e2-bf34-86aba36c324a.mq.ap-northeast-2.amazonaws.com:5671"),
-    Uri = new Uri("amqp://ec2-3-34-123-235.ap-northeast-2.compute.amazonaws.com:5672"),
+    // Uri = new Uri("amqp://ec2-3-34-123-235.ap-northeast-2.compute.amazonaws.com:5672"),
+    Uri = new Uri("amqps://b-5ffc876c-f4a1-44e5-a730-9fcd7112eee4.mq.ap-northeast-2.amazonaws.com:5671"),
     UserName = "admin",
-    // UserName = "pf",
-    Password = "Cokee"
-    // Password = "123$mqTest123$"
+    // UserName = "admin",
+    Password = "ejrrpdlawm1!"
+    // Password = "Cokee"
 };
+
+// var queue = "demo-queue";
+var queue = "queue_game_info";
+
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
 channel.ConfirmSelect();
 channel.WaitForConfirmsOrDie();
 Console.WriteLine("Connected to " + factory.Uri);
@@ -31,7 +35,9 @@ consumer.Received += (sender, e) =>
     Console.WriteLine(message);
 };
 
-channel.BasicConsume("demo-queue", true, consumer);
+channel.BasicConsume(queue, true, consumer);
+
+// Queue Message Ex) {"header":{"cmd":"StartGame"},"body":{"deviceCode":10001,"gameCode":1780}}
 
 while(true)
 {
