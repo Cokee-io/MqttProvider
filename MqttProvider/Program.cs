4b95304e9dbe59a33b3061d0686fead37b804d04
Yeon-Taek Choi <ohe@cokee.io>
2022-10-18 01:25:35 +0900

    change connection info / add sslOption for mqtt.cokee.io

--- MqttProvider/Program.cs
+++ MqttProvider/Program.cs
@@ -5,18 +5,26 @@ using RabbitMQ.Client;
 var builder = WebApplication.CreateBuilder(args);
 var factory = new ConnectionFactory
 {
-    Uri = new Uri("amqps://b-9c19991c-d773-47e2-bf34-86aba36c324a.mq.ap-northeast-2.amazonaws.com:5671"),
-    UserName = "pf",
-    Password = "123$mqTest123$"
+    Uri = new Uri("amqps://mqtt.cokee.io:5671"),
+    // Uri = new Uri("amqps://b-5ffc876c-f4a1-44e5-a730-9fcd7112eee4.mq.ap-northeast-2.amazonaws.com:5671"),
+    UserName = "admin",
+    // UserName = "admin",
+    // Password = "ejrrpdlawm1!"
+    Password = "Cokee",
+    Ssl = new SslOption("mqtt.cokee.io", "", enabled:true)
 };
+
+var queue = "100007-reservation";
+// var queue = "queue_game_info";
+
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare("demo-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-var message = new { name = "Producer", Message = "Hello" };
+channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false, arguments: null);
+var message = new { name = "Producer", message = "Hello" };
 var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
 
-channel.BasicPublish("", "demo-queue", null, body);
+channel.BasicPublish("", queue, null, body);
 // Add services to the container.
 
 builder.Services.AddControllers();
--- RabbitMq.Cosumer/Program.cs
+++ RabbitMq.Cosumer/Program.cs
@@ -6,21 +6,22 @@ using RabbitMQ.Client.Events;
 Console.WriteLine("Connecting . . .");
 var factory = new ConnectionFactory
 {
-    // Uri = new Uri("amqp://ec2-3-34-123-235.ap-northeast-2.compute.amazonaws.com:5672"),
-    Uri = new Uri("amqps://b-5ffc876c-f4a1-44e5-a730-9fcd7112eee4.mq.ap-northeast-2.amazonaws.com:5671"),
+    Uri = new Uri("amqps://mqtt.cokee.io:5671"),
+    // Uri = new Uri("amqps://b-5ffc876c-f4a1-44e5-a730-9fcd7112eee4.mq.ap-northeast-2.amazonaws.com:5671"),
     UserName = "admin",
     // UserName = "admin",
-    Password = "ejrrpdlawm1!"
-    // Password = "Cokee"
+    // Password = "ejrrpdlawm1!"
+    Password = "Cokee",
+    Ssl = new SslOption("mqtt.cokee.io", "", enabled:true)
 };
 
-// var queue = "demo-queue";
-var queue = "queue_game_info";
+var queue = "100007-reservation";
+// var queue = "queue_game_info";
 
 using var connection = factory.CreateConnection();
 using var channel = connection.CreateModel();
 
-channel.QueueDeclare(queue, durable: false, exclusive: false, autoDelete: false, arguments: null);
+channel.QueueDeclare(queue, durable: true, exclusive: false, autoDelete: false, arguments: null);
 channel.ConfirmSelect();
 channel.WaitForConfirmsOrDie();
 Console.WriteLine("Connected to " + factory.Uri);
